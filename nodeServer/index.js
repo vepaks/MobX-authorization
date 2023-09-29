f9f3567096fa0b08ab5ece77a7a5cf7ecbf9a5aa
Aleksandar Kamburov <kamburoff@gmail.com>
2023-09-30 01:01:52 +0300

    add mongodb

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,13 +1,21 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
-const e = require("express");
+const mongoose = require('mongoose')
 
 const app = express()
-const PORT = 5000
+const PORT =  process.env.PORT || 3000
 
 const start = async () => {
   try {
+      await mongoose.connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+
+      }).then(() => console.log("Connected to database"))
+          .catch((err) => console.log("DB Error", err));
+
     app.listen(PORT, () => console.log(`Server running http://localhost:${PORT}...`))
   } catch (e) {
       console.log(e)
@@ -15,3 +23,5 @@ const start = async () => {
 }
 
 start()
+
+
