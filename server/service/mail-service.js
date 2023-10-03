c339ff4102ac73da71bfa1f01eaa6e92b948c8a9
Aleksandar Kamburov <kamburoff@gmail.com>
2023-10-03 09:58:55 +0300

    update mail service

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -1,30 +1,35 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 class MailService {
+
   constructor() {
     this.transporter = nodemailer.createTransport({
+      service: 'gmail',
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-      },
-    });
+        pass: process.env.SMTP_PASSWORD
+      }
+    })
   }
 
+  
   async sendActivationMail(to, link) {
     await this.transporter.sendMail({
       from: process.env.SMTP_USER,
       to,
-      subject: "Активиране на акаунт на " + process.env.API_URL,
-      text: "",
+      subject: 'Активация аккаунта на ' + process.env.API_URL,
+      text: '',
       html:
-         ` <div>
-              <h1>За да активирате акаунта отидете на този линк:</h1>
-                  <a href=${link}">${link}</a>
-          </div>`,
-    });
+          `
+                    <div>
+                        <h1>За да активирате акаунта кликнете на линка</h1>
+                        <a href="${link}">${link}</a>
+                    </div>
+                `
+    })
   }
 }
 
